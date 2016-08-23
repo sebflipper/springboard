19a29abd54bbd3a4e39827c62b79802f5fac2746
Joe Rickerby <joerick@mac.com>
2016-08-23 10:59:11 +0100

    Simplify get_network_icon_name code

--- springboard.tingapp/icon_utils.py
+++ springboard.tingapp/icon_utils.py
@@ -12,12 +12,11 @@ def get_network_icon_name(cell):
             quality = min(99, max(cell.link_quality * 100 / 70, 0))
         else:
             quality = min(99, max((110+cell.signal) * 100 / 70, 0))
-    else:
-        if tingbot.get_ip_address():
-            quality = -1  # wired network
-        else:
-            return 'WiFi-1.png'
-    if quality == -1:
+            
+        return 'WiFi-%d.png' % (int(quality / 20) + 2)
+    elif tingbot.get_ip_address():
+        # has network connection that's not wifi, must be ethernet
         return 'Ethernet-1.png'
     else:
-        return 'WiFi-%d.png' % (int(quality / 20) + 2)
+        # has no network connection
+        return 'WiFi-1.png'
